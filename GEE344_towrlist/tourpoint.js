c1c82275edb814664370e23769421f9619b8ac52
Hinnicon <158019742+Hinnicon@users.noreply.github.com>
2024-05-15 12:37:58 +0700

    Add files via upload

---
+++ GEE344_towrlist/tourpoint.js
@@ -0,0 +1,52 @@
+var tourpoint =
+{
+
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Yai National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.50210599213818,
+            14.339589259827349
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palace",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.61848126717354,
+            13.81187317306059
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Icon Siam",
+          "type": "cutral",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.51044293369455,
+            13.726740011885411
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
